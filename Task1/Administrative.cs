1fbbc6f08814b85a617471788ab403819d7d1db4
terreror15-wq <terreror15@gmail.com>
2026-06-09 17:49:40 -0400

    These classes have inheritance.

---
+++ Task1/Administrative.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2HomeWork1.Models
+{
+    public class Administrative : Employee
+    {
+        public string Departament { get; set; }
+    }
+}

673503bfb444182550c46c3eee088f3bde14d059
terreror15-wq <terreror15@gmail.com>
2026-06-09 17:51:53 -0400

    I added more classes whit inherance.

---
+++ Task1/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2HomeWork1.Models
+{
+    public class Employee : ComunityMembers
+    {
+        public string Roll { get; set; }
+        public double Salary{ get; set; }
+        public string Address { get; set; }
+
+    }
+}

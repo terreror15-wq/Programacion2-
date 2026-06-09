f5743d8d93cb0ceeb9b6d6dbd31b58ae4ab80e92
terreror15-wq <terreror15@gmail.com>
2026-06-09 17:55:23 -0400

    These classes have interactions

---
+++ Task1/Maestro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2HomeWork1.Models
+{
+    public class Maestro : Teacher
+    {
+        public string Curso { get; set; }
+    }
+}

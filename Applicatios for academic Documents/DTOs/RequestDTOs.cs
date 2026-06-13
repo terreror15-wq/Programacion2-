aa4e0812dfdaca23c60ac012556531dd7bad153d
terreror15-wq <terreror15@gmail.com>
2026-06-13 15:08:59 -0400

    Adding DTOs

---
+++ Applicatios for academic Documents/DTOs/RequestDTOs.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Tarea_1.DTOs
+{
+    public class RequestDTOs
+    {
+        [Required]
+        [JsonIgnore]
+        public int Id { get; set; }
+        [Required]
+        public DateTime CreateDate { get; set; }
+        [Required]
+        public DateTime DeliveredTime { get; set; }
+        [Required]
+        public string RequestStatus { get; set; }
+    }
+}

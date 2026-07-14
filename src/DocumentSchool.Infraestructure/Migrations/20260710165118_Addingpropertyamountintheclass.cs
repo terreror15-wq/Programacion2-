a58f6a9c3623664b7a16b07c59679f57fbcac263
terreror15-wq <terreror15@gmail.com>
2026-07-14 17:26:23 -0400

    Add files via upload

---
+++ src/DocumentSchool.Infraestructure/Migrations/20260710165118_Addingpropertyamountintheclass.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentSchool.InfraEstructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addingpropertyamountintheclass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Amount",
+                table: "Registers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Registers");
+        }
+    }
+}

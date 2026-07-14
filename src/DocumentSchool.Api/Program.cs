8a2a0f8ba23ea1dbb4be8064e174d1edba23d1c6
terreror15-wq <terreror15@gmail.com>
2026-07-14 17:39:51 -0400

    Add files via upload

--- src/DocumentSchool.Api/Program.cs
+++ src/DocumentSchool.Api/Program.cs
@@ -1,15 +1,36 @@
+using DocumentSchool.Domain.Interfaces;
+using DocumentSchool.InfraEstructure.Context;
+using DocumentSchool.InfraEstructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var MyConectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DocumentSchoolDbContext>(x =>
+{
+    x.UseNpgsql(MyConectionString);
 
+});
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IStundentRepository, StudentRepository>();
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
+builder.Services.AddScoped<IRequestRepesitory, RequestRepository>();
+builder.Services.AddScoped<IRegistrerRepository, RegistrerRepository>();
 
 var app = builder.Build();
+app.MapSwagger();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 
-app.UseHttpsRedirection();
+
+
+
+
 
 
 

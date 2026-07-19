23c641e6f08360b66fa9b559cd1ea064e59d8bac
terreror15-wq <terreror15@gmail.com>
2026-07-18 22:47:42 -0400

    Adding properties

--- src/DocumentSchool.Domain/Entities/Document.cs
+++ src/DocumentSchool.Domain/Entities/Document.cs
@@ -2,5 +2,9 @@
 {
     public class Docment
     {
+        public string NameDocument { get; set; }
+
+        public int RequestId { get; set; }
+        public Request Request { get; set; }
     }
 }

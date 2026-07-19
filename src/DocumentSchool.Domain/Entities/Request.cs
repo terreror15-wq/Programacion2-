a0f9f919e656d36e19a1f88284f2e71210a9f0c6
terreror15-wq <terreror15@gmail.com>
2026-07-18 22:50:41 -0400

    Adding properties

--- src/DocumentSchool.Domain/Entities/Request.cs
+++ src/DocumentSchool.Domain/Entities/Request.cs
@@ -1,6 +1,14 @@
 ﻿namespace DocumentSchool.Entities
 {
-    public class Request
+    public class Request : BaseEntity
     {
+         public DateTime CreatedAt { get; set; }
+
+         public string Status { get; set; }
+
+         public int StudentId { get; set; }
+         public Student Student { get; set; }
+
+        public ICollection<Document> Documents { get; set; } 
     }
 }

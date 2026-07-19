e42af926798441143ecc429502cce4964173ec7b
terreror15-wq <terreror15@gmail.com>
2026-07-18 22:49:08 -0400

    Adding porperties

--- src/DocumentSchool.Domain/Entities/Registrer.cs
+++ src/DocumentSchool.Domain/Entities/Registrer.cs
@@ -2,5 +2,11 @@
 {
     public class Registrer
     {
+         public DateTime CreatedAt { get; set; }
+        public DateTime? DeliveredAt { get; set; }
+        public int TotalDocuments { get; set; }
+        public int Amount { get; set; }
+        public int RequestId { get; set; }
+        public Request Request { get; set; }
     }
 }

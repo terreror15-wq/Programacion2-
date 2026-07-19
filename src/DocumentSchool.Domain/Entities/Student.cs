9daae4956968c7fe4f6dcfa7df9b80863c349f0f
terreror15-wq <terreror15@gmail.com>
2026-07-18 22:52:48 -0400

    Adding properties ti Studentn's class

--- src/DocumentSchool.Domain/Entities/Student.cs
+++ src/DocumentSchool.Domain/Entities/Student.cs
@@ -1,6 +1,13 @@
 ﻿namespace DocumentSchool.Entities
 {
-    public class Student
+    public class Student : BaseEntity
     {
+    public string Name { get; set; }
+    public string LastNAme { get; set; }
+    public string Tel { get; set; }
+    public int Age { get; set; }
+    public string Grade { get; set; }
+
+     public ICollection<Request> Requests { get; set; } 
     }
 }
